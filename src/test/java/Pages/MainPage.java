5ea41f4f2aa8103224129efc69c7b2ddcef97834
shubhamkumar-deloitte <shubhamkumar-deloitte@users.noreply.github.com>
2022-03-27 16:13:27 +0530

    read customer details from excel till now

--- src/test/java/Pages/MainPage.java
+++ src/test/java/Pages/MainPage.java
@@ -1,7 +1,89 @@
 package Pages;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class MainPage {
-    public static void main(String[] args) {
-        System.out.println("hello world");
+
+    WebDriver driver;
+    By bankManagerLoginBtn=By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/button");
+    By addCustomerBtn=By.xpath("//div[@class='center']/child::button[1]");
+    By customerFirstName=By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[1]/input");
+    By customerLastName=By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[2]/input");
+    By customerPostCode=By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[3]/input");
+    String customerFirstNameValue;
+    String customerLastnameValue;
+    double customerPostCodevalue;
+
+
+    public MainPage(WebDriver driver){
+        this.driver=driver;
     }
+
+    public void bankManagerLoginClick(){
+        WebElement bankManagerLoginElement=driver.findElement(bankManagerLoginBtn);
+        bankManagerLoginElement.click();
+    }
+    public void addCustomerBtnClick(){
+        WebElement addCustomerBtnElement= driver.findElement(addCustomerBtn);
+        addCustomerBtnElement.click();
+    }
+    public void readCustomerDetails() throws IOException {
+
+        String excelFilePath="C:\\Users\\shubhamkumar32\\Desktop\\MAIN_ASSIGNMENT_EXCEL_DATA\\Customer_Details.xlsx";
+        FileInputStream fis=new FileInputStream(excelFilePath);
+        XSSFWorkbook workbook=new XSSFWorkbook(fis);
+        XSSFSheet sheet=workbook.getSheetAt(0);
+        XSSFRow row=null;
+        XSSFCell cell=null;
+
+        //iterate over all the rows and columns present in the sheet
+        for(int i=1;i<sheet.getLastRowNum();i++)
+        {
+            row=sheet.getRow(i);
+            for(int j=0;j<row.getLastCellNum();j++)
+            {
+                cell=row.getCell(j);
+                if(j==0)
+                {
+                    customerFirstNameValue=cell.getStringCellValue();
+                }
+                if(j==1){
+                    customerLastnameValue=cell.getStringCellValue();
+                }
+                if(j==2){
+                    customerPostCodevalue=cell.getNumericCellValue();
+                }
+            }
+        }
+
+        System.out.println("printing customer details");
+        System.out.println("fisrt name is "+customerFirstNameValue);
+        System.out.println("last name is "+customerLastnameValue);
+        System.out.println("postal code is"+customerPostCodevalue);
+
+    }
+    public void addCustomerDetails(){
+        WebElement customerFisrtnameElement= driver.findElement(customerFirstName);
+        WebElement customerLastnameElement= driver.findElement(customerLastName);
+        WebElement customerPostCodeElement= driver.findElement(customerPostCode);
+        customerFisrtnameElement.sendKeys("Shubham");
+        customerLastnameElement.sendKeys("Kumar");
+        customerPostCodeElement.sendKeys("852201");
+    }
+
+
 }
+
+//Shubham
+//Kumar
+//560064
---
+++ src/test/java/Tests/LoginPageTest.java
@@ -0,0 +1,65 @@
+package Tests;
+
+import Pages.MainPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class LoginPageTest {
+
+    WebDriver driver;
+
+    String HomeUrl = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login";
+    MainPage mainPage;
+
+    @BeforeSuite
+    public void setup() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\shubhamkumar32\\Downloads\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(HomeUrl);
+        Thread.sleep(2000);
+    }
+
+    @Test(priority = 1)
+    public void bankManagerLogin() throws InterruptedException {
+
+        mainPage = new MainPage(driver);
+        mainPage.bankManagerLoginClick();
+        Thread.sleep(2000);
+        //driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/button")).click();
+    }
+
+    @Test(priority = 2)
+    public void addCustomerBtnClick() throws InterruptedException {
+        mainPage = new MainPage(driver);
+        mainPage.addCustomerBtnClick();
+        Thread.sleep(2000);
+
+        //driver.findElement(By.xpath("//div[@class='center']/child::button[1]")).click();
+    }
+    @Test(priority = 3)
+    public void addCustomerDetails() throws IOException {
+
+        //driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[1]/input")).click();
+        //driver.findElement(By.xpath("//input[@class='form-control ng-pristine ng-invalid ng-invalid-required ng-touched']")).sendKeys("shubham");
+//        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[1]/input")).sendKeys("sadas");
+//        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[2]/input")).sendKeys("shjcbsd");
+//        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[2]/div/div/form/div[3]/input")).sendKeys("sdcsh");
+
+        mainPage=new MainPage(driver);
+
+        mainPage.addCustomerDetails();
+        mainPage.readCustomerDetails();
+
+    }
+//    @AfterSuite
+//    public void tearDown(){
+//        driver.quit();
+//    }
+}
